730be20ca2075346a08380382e4b452dfbf9f9fc
danilogoulart <danilo@outlook.com.br>
2017-07-21 21:13:27 -0300

    Alterações nas views

--- application/config/database.php
+++ application/config/database.php
@@ -79,7 +79,7 @@ $db['default'] = array(
 	'username' => 'danilo',
 	'password' => '304460',
 	'database' => 'danilo',
-	'dbdriver' => 'pdo',
+	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
